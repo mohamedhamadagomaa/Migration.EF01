e4a3661f784aa15cd5f8ec24c9fd55d6fca2dc0b
Mohamed Hamada <moha2016226@gmail.com>
2024-10-25 05:49:52 -0700

    create UsingDbContextPooling Project

---
+++ EF.UsingContextPooling/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EF.UsingDbContextPooling.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.UsingDbContextPooling.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Wallet> Wallets { get; set; } = null!;
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ EF.UsingContextPooling/Program.cs
@@ -0,0 +1,28 @@
+﻿using EF.UsingDbContextPooling.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EF.UsingDbContextPooling
+{
+    internal class Program
+    {
+        static AppDbContext context;
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build();
+
+            var constr = configuration.GetSection("constr").Value;
+            var services = new ServiceCollection();
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(constr));
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            using (var context = serviceProvider.GetService<AppDbContext>())
+            {
+                foreach (var wallet in context!.Wallets)
+                    Console.WriteLine(wallet);
+            }
+            Console.ReadKey();
+        }
+    }
+}
