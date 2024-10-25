884bb901e1d7e9a36ae7754ecef63c50f803c9d0
Mohamed Hamada <moha2016226@gmail.com>
2024-10-25 01:33:22 -0700

    create DependencyInjection proj

---
+++ EF.UsingDependencyInjection/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EF.UsingDependencyInjection.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.UsingDependencyInjection.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Wallet> Wallets { get; set; } = null!;
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ EF.UsingDependencyInjection/Program.cs
@@ -0,0 +1,26 @@
+﻿using EF.UsingDependencyInjection.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EF.UsingDependencyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build();
+
+            var constr = configuration.GetSection("constr").Value;
+            var services = new ServiceCollection();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(constr));
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            using (var context = serviceProvider.GetService<AppDbContext>())
+            {
+                foreach (var wallet in context!.Wallets)
+                    Console.WriteLine(wallet.Holder);
+            }
+        }
+    }
+}
