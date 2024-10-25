4461a6f08311adbf8584b509e1b3c34e3a274bc9
Mohamed Hamada <moha2016226@gmail.com>
2024-10-25 01:43:34 -0700

    create DbcontextFactory Project

---
+++ UsingContextFactory/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EF.UsingContextFactory.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.UsingContextFactory.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Wallet> Wallets { get; set; } = null!;
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ UsingContextFactory/Program.cs
@@ -0,0 +1,26 @@
+﻿using EF.UsingContextFactory.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EF.UsingContextFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build();
+
+            var constr = configuration.GetSection("constr").Value;
+            var services = new ServiceCollection();
+            services.AddDbContextFactory<AppDbContext>(options => options.UseSqlServer(constr));
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+            var contextFactory = serviceProvider.GetService<IDbContextFactory<AppDbContext>>();
+            using (var context = contextFactory!.CreateDbContext())
+            {
+                foreach (var wallet in context!.Wallets)
+                    Console.WriteLine(wallet.Holder);
+            }
+        }
+    }
+}
