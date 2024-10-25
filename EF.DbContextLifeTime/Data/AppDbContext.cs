24483e5ded8eb765709d5c2072197dcd846187cf
Mohamed Hamada <moha2016226@gmail.com>
2024-10-25 03:07:18 -0700

    create DbcontextLifeTime project

---
+++ EF.DbContextLifeTime/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EF.DbContextLifeTime.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.DbContextLifeTime.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Wallet> Wallets { get; set; } = null!;
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ EF.DbContextLifeTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using EF.DbContextLifeTime.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EF.DbContextLifeTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build();
+
+            var constr = configuration.GetSection("constr").Value;
+            var optionBuilder = new DbContextOptionsBuilder();
+            optionBuilder.UseSqlServer(constr);
+            var option = optionBuilder.Options;
+            using (var context = new AppDbContext(option))
+            {
+                // THere are two wallets were added 
+                //var w1 = new Wallet { Id = 16, Holder = "Jassem", Balance = 1000m };
+                //context.Wallets.Add(w1);
+                //var w2 = new Wallet { Id = 17, Holder = "Rania", Balance = 15000m };
+                //context.Wallets.Add(w2);
+                //context.SaveChanges();
+            }
+        }
+    }
+}
